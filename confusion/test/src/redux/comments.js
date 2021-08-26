2ed7f7e83ae707e526e5b542a8d0edc5a5c5ec04
doaa35 <doaa35621@gmail.com>
2021-08-26 12:25:21 +0200

    Combining Reducers

---
+++ confusion/test/src/redux/comments.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from '../shared_Data/comments';
+
+export const Comments = (state = COMMENTS, action) => {
+    switch (action.type) {
+
+        default:
+          return state;
+      }
+};
